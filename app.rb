d33716e761640197a8a1d3d026d2210c415b8a3b
Rainer Borene <rainerborene@gmail.com>
2015-04-24 12:57:38 -0300

    Add Materialize UI framework.

--- app.rb
+++ app.rb
@@ -7,9 +7,15 @@ class Chat < Sinatra::Base
   set :sprockets, Sprockets::Environment.new(root)
 
   configure do
+    sprockets.append_path 'static/templates'
     sprockets.append_path 'static/javascripts'
     sprockets.append_path 'static/stylesheets'
     sprockets.append_path 'static/images'
+
+    sprockets.append_path 'vendor/javascripts'
+    sprockets.append_path 'vendor/stylesheets'
+    sprockets.append_path 'vendor/images'
+
     sprockets.cache = Sprockets::Cache::FileStore.new('./tmp')
   end
 
--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -1,1 +1,4 @@
+//= require_tree ../templates
+//= require_self
+
 alert('it works');
